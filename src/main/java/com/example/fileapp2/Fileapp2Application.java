be791f7c0a50b57b4dbe2ee25635324c26afcdb9
LifeIsOne <didtmdgh258@gmail.com>
2024-02-29 11:23:25 +0900

    사진 업로드 (해쉬)

---
+++ src/main/java/com/example/fileapp2/Fileapp2Application.java
@@ -0,0 +1,13 @@
+package com.example.fileapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fileapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fileapp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fileapp2/Fileapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fileapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
